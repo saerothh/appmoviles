0bab6ba9f57578455e29d1881816de0563f159a6
saeroth <saeroth@gmail.com>
2015-11-28 00:44:03 +0100

    commit2

--- proyecto/app1/views.py
+++ proyecto/app1/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Movil
 
 # Create your views here.
 def post_list(request):
-        return render(request, 'app1/post_list.html', {})
+        moviles = Movil.objects.filter(fecha_lanzamiento__lte=timezone.now()).order_by('fecha_lanzamiento')
+        return render(request, 'app1/post_list.html',{'moviles': moviles})
