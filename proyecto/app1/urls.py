acbc39f3596fa1b9d0bae31232556eac159cfb24
saeroth <saeroth@gmail.com>
2015-11-27 23:26:27 +0100

    html

--- proyecto/app1/urls.py
+++ proyecto/app1/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('app1.urls')),
+    url(r'^$', views.post_list),
 ]
