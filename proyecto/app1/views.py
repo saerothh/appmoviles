7628adc76cca25435e4009326cf29f73f2178d7a
saeroth <saeroth@gmail.com>
2015-11-29 22:15:06 +0100

    modifi

--- proyecto/app1/views.py
+++ proyecto/app1/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.utils import timezone
 from .models import Movil
 from django.shortcuts import render, get_object_or_404
